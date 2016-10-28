d94a553a03a8721fe6e7b3da7f06c579b8c35b9b
atxutegi <eatxutegi001@gmail.com>
2016-10-28 18:21:01 +0200

    allow input parameters from scheduler

--- maril-in-monroe-plus-plus/files/executor2.sh
+++ maril-in-monroe-plus-plus/files/executor2.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 cd /opt/monroe
 #java maril.client.marilClientLaunch -d3 -h158.227.68.19 -p3446 -t3
+duration=`echo $1 | sed -e $'s/,/\\\n/g' | grep duration | cut -d: -f2 | sed 's/"//g'`
+host=`echo $1 | sed -e $'s/,/\\\n/g' | grep host | cut -d: -f2 | sed 's/"//g'`
+port=`echo $1 | sed -e $'s/,/\\\n/g' | grep port | cut -d: -f2 | sed 's/"//g'`
+threads=`echo $1 | sed -e $'s/,/\\\n/g' | grep threads | cut -d: -f2 | sed 's/"//g'`
+preTest=`echo $1 | sed -e $'s/,/\\\n/g' | grep preTest | cut -d: -f2 | sed 's/"//g'`
 eval `ssh-agent`
 ssh-add id_rsa
 ssh -o "StrictHostKeyChecking no" operario@mad.velocimetro.org 'sh maril-in-monroe-TCPserver/testLauncher.sh `wget http://ipinfo.io/ip -qO -` `date '+%F_%H-%M-%S'`  < /dev/null > /dev/null 2>&1'
-#python experiment4.py &
+python experimentTXT.py &
 #java maril.client.marilClientLaunch -d5 -h85.17.254.6 -p3446 -t3
-java maril.client.marilClientLaunch -d5 -h212.81.134.74 -p3446 -t3
+#java maril.client.marilClientLaunch -d5 -h212.81.134.74 -p3446 -t3
+java maril.client.marilClientLaunch -d$duration -h$host -p$port -t$threads -pre $preTest
 ssh -o "StrictHostKeyChecking no" operario@mad.velocimetro.org 'sh maril-in-monroe-TCPserver/testKiller.sh'
 cp outputData.txt /monroe/results/
--- maril-in-monroe-plus/files/executor.sh
+++ maril-in-monroe-plus/files/executor.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd /opt/monroe
-#java maril.client.marilClientLaunch -d3 -h158.227.68.19 -p3446 -t3
-java maril.client.marilClientLaunch -d5 -h85.17.254.6 -p3446 -t3
+#"key":number,"key":"string"
+duration=`echo $1 | sed -e $'s/,/\\\n/g' | grep duration | cut -d: -f2 | sed 's/"//g'`
+host=`echo $1 | sed -e $'s/,/\\\n/g' | grep host | cut -d: -f2 | sed 's/"//g'`
+port=`echo $1 | sed -e $'s/,/\\\n/g' | grep port | cut -d: -f2 | sed 's/"//g'`
+threads=`echo $1 | sed -e $'s/,/\\\n/g' | grep threads | cut -d: -f2 | sed 's/"//g'`
+preTest=`echo $1 | sed -e $'s/,/\\\n/g' | grep preTest | cut -d: -f2 | sed 's/"//g'`
+
+echo "$duration $host $port $threads $preTest"
+
+#java maril.client.marilClientLaunch -d3 -h158.227.68.19 -p3446 -t3 -pre true
+#java maril.client.marilClientLaunch -d5 -h85.17.254.6 -p3446 -t3 -pre true
+java maril.client.marilClientLaunch -d$duration -h$host -p$port -t$threads -pre $preTest
 cp outputData.txt /monroe/results/
